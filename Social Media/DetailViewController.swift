b53a172a4ae09190dd81099d5ca5bde4b57bf101
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-05-14 21:59:15 -0700

    Adding UIActivityViewController code

--- Social Media/DetailViewController.swift
+++ Social Media/DetailViewController.swift
@@ -19,6 +19,9 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Picture \(selectedPictureNumber) of \(totalPictures)"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         // Do any additional setup after loading the view.
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -35,4 +38,13 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
 
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 }

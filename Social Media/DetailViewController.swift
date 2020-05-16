b0003ae832576a43dac3ffc19b5c4901fc1f6245
Farhana Mustafa <farhana.x.mustafa@kp.org>
2020-05-16 08:42:15 -0700

    Adding image name to list of items that are shared

--- Social Media/DetailViewController.swift
+++ Social Media/DetailViewController.swift
@@ -43,7 +43,7 @@ class DetailViewController: UIViewController {
             print("No image found")
             return
         }
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        let vc = UIActivityViewController(activityItems: [image, selectedImage as Any], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
